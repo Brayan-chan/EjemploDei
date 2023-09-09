8a5075cc00a0b72a81ef3243b12176694f9efdd6
Brayan-chan <113928216+Brayan-chan@users.noreply.github.com>
2023-09-08 23:11:02 -0600

    Commit 1

---
+++ src/ejemplodei/EjemploDei.java
@@ -0,0 +1,11 @@
+package ejemplodei;
+
+
+public class EjemploDei {
+
+    public static void main(String[] args) {
+        System.out.println("Esto es un ejemplo de historial de versiones");
+        System.out.println("Creemos una clase pues para checar que onda");
+    }
+    
+}
